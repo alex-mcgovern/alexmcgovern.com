87de7d01537655ee221e8a0f0afddff0429cef6c
alex-mcgovern <alex_mcgovernsmith@icloud.com>
2024-02-29 21:08:14 +0000

    setup basic homepage

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,19 @@
-import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
+import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
+import { createVanillaExtractPlugin } from "@vanilla-extract/next-plugin";
+
+const withVanillaExtract = createVanillaExtractPlugin({
+    identifiers: "short",
+    outputCss: true,
+});
 
 // Here we use the @cloudflare/next-on-pages next-dev module to allow us to use bindings during local development
 // (when running the application with `next dev`), for more information see:
 // https://github.com/cloudflare/next-on-pages/blob/5712c57ea7/internal-packages/next-dev/README.md
-if (process.env.NODE_ENV === 'development') {
-  await setupDevPlatform();
+if (process.env.NODE_ENV === "development") {
+    await setupDevPlatform();
 }
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+export default withVanillaExtract(nextConfig);
